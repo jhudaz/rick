5f6178de9b5e93e6ff356527d52ae45df8151a6e
Jaime Velez <jandres.vr@hotmail.com>
2019-02-16 09:24:44 -0500

    react-router-dom, redux, node-sass were add to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './App.scss';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <h1>hola</h1>
       </div>
     );
   }
